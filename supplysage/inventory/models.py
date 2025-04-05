5d5569824bad3db6bf3385d5e9972e470a630bad
Patrick Kelly <pkelly@babelstreet.com>
2025-04-04 23:00:20 -0400

    Updated the Item model to include categories.

--- supplysage/inventory/models.py
+++ supplysage/inventory/models.py
@@ -3,9 +3,18 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    CATEGORY_CHOICES = [
+        ('ELECTRONICS', 'Electronics'),
+        ('FURNITURE', 'Furniture'),
+        ('CLOTHING', 'Clothing'),
+        ('FOOD', 'Food'),
+        ('OTHER', 'Other'),
+    ]
+
     name = models.CharField(max_length=100, unique=True)
     quantity = models.PositiveIntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='OTHER')
 
     def __str__(self):
         return f'{self.name} - (Qty: {self.quantity})'
